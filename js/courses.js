662c296ddfc8287ce082098a8af2d482ae89232a
doniyor-eshmamatov <edoniyorbekdev@gmail.com>
2023-05-14 18:00:08 +0500

    Finished main page

---
+++ js/courses.js
@@ -0,0 +1,25 @@
+const modeToggler = document.querySelector('.header-top-mode');
+const body = document.querySelector('body');
+
+modeToggler.addEventListener('click', () => {
+  body.classList.toggle('dark');
+});
+
+
+const modeTogglerMobile = document.querySelector('#header-top-mode');
+
+modeTogglerMobile.addEventListener('click', () => {
+  body.classList.toggle('dark');
+});
+
+const openMenu = document.querySelector('.open-menu');
+const closeMenu = document.querySelector('.menu-close');
+const menu = document.querySelector('.nav-mobile');
+
+openMenu.addEventListener('click', () => {
+  menu.classList.add('open');
+});
+
+closeMenu.addEventListener('click', () => {
+  menu.classList.remove('open');
+});
