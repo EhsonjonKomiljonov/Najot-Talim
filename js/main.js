15a8be3aebe3860f5cd5d4ac8b5ec83a9931fd03
AbdullohKomil <akomiljonov@icloud.com>
2023-05-14 16:14:12 +0500

    jobs

--- js/main.js
+++ js/main.js
@@ -1,8 +1,78 @@
-
-
 const modeToggler = document.querySelector('.header-top-mode');
 const body = document.querySelector('body');
 
 modeToggler.addEventListener('click', () => {
-    body.classList.toggle('dark')
-})
+  body.classList.toggle('dark');
+});
+
+const dataJobs = [
+  {
+    src: './images/epam.svg',
+  },
+  {
+    src: './images/itechart.svg',
+  },
+  {
+    src: './images/humo.svg',
+  },
+  {
+    src: './images/agrobank.svg',
+  },
+  {
+    src: './images/davrbank.svg',
+  },
+  {
+    src: './images/texnomart.svg',
+  },
+  {
+    src: './images/soliq.svg',
+  },
+  {
+    src: './images/uzcard.svg',
+  },
+  {
+    src: './images/my-taxi.svg',
+  },
+  {
+    src: './images/mfaktor.svg',
+  },
+  {
+    src: './images/jafton.svg',
+  },
+  {
+    src: './images/fido-studio.svg',
+  },
+  {
+    src: './images/osmondagi-bolalar.svg',
+  },
+  {
+    src: './images/islom-uz.svg',
+  },
+  {
+    src: './images/cambridge.svg',
+  },
+  {
+    src: './images/kunuz.svg',
+  },
+  {
+    src: './images/yoshlar-ishlari.svg',
+  },
+  {
+    src: './images/pcg.svg',
+  },
+];
+
+const jobsList = document.querySelector('.jobs__list');
+
+const renderJobs = () => {
+  for (el of dataJobs) {
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+    img.setAttribute('src', el.src);
+    li.appendChild(img);
+    jobsList.appendChild(li);
+    console.log(li);
+  }
+};
+
+renderJobs();
