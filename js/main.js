f72748aac0c350a5ace489a577aa2d161b2db9eb
EhsonjonKomiljonov <ekomiljonov29@gmail.com>
2023-05-14 17:41:53 +0500

    add comments section

--- js/main.js
+++ js/main.js
@@ -1,8 +1,102 @@
-
-
 const modeToggler = document.querySelector('.header-top-mode');
 const body = document.querySelector('body');
 
 modeToggler.addEventListener('click', () => {
-    body.classList.toggle('dark')
-})
+  body.classList.toggle('dark');
+});
+
+const firstVideo = document.querySelector('.first');
+const secondVideo = document.querySelector('.second');
+const thirdVideo = document.querySelector('.third');
+const fourthVideo = document.querySelector('.fourth');
+const firstButton = document.querySelector('.first-btn');
+const firstPause = document.querySelector('.first-pause');
+const secondButton = document.querySelector('.second-btn');
+const secondPause = document.querySelector('.second-pause');
+const thirdButton = document.querySelector('.third-btn');
+const thirdPause = document.querySelector('.third-pause');
+const fourthButton = document.querySelector('.fourth-btn');
+const fourthPause = document.querySelector('.fourth-pause');
+
+firstButton.addEventListener('click', () => {
+  firstVideo.play();
+  secondVideo.pause();
+  thirdVideo.pause();
+  fourthVideo.pause();
+  firstPause.style.display = 'flex';
+  firstButton.style.display = 'none';
+});
+
+firstPause.addEventListener('click', () => {
+  firstVideo.pause();
+  firstButton.style.display = 'flex';
+  firstPause.style.display = 'none';
+});
+
+secondButton.addEventListener('click', () => {
+  secondVideo.play();
+  firstVideo.pause();
+  thirdVideo.pause();
+  fourthVideo.pause();
+  secondPause.style.display = 'flex';
+  secondButton.style.display = 'none';
+});
+
+secondPause.addEventListener('click', () => {
+  secondVideo.pause();
+  secondButton.style.display = 'flex';
+  secondPause.style.display = 'none';
+});
+
+thirdButton.addEventListener('click', () => {
+  thirdVideo.play();
+  firstVideo.pause();
+  secondVideo.pause();
+  fourthVideo.pause();
+  thirdPause.style.display = 'flex';
+  thirdButton.style.display = 'none';
+});
+
+thirdPause.addEventListener('click', () => {
+  thirdVideo.pause();
+  thirdButton.style.display = 'flex';
+  thirdPause.style.display = 'none';
+});
+
+fourthButton.addEventListener('click', () => {
+  fourthVideo.play();
+  firstVideo.pause();
+  secondVideo.pause();
+  thirdVideo.pause();
+  fourthPause.style.display = 'flex';
+  fourthButton.style.display = 'none';
+});
+
+fourthPause.addEventListener('click', () => {
+  fourthVideo.pause();
+  fourthButton.style.display = 'flex';
+  fourthPause.style.display = 'none';
+});
+
+const carouselList = document.querySelector('.videos');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+nextBtn.addEventListener('click', () => {
+  prevBtn.style.backgroundColor = '#ba8d5b';
+  console.log(carouselList.scrollLeft)
+  if (carouselList.scrollLeft >= 466.3999938964844) {
+    nextBtn.style.backgroundColor = '#333';
+  } else {
+    carouselList.scrollLeft += 215;
+  }
+});
+
+prevBtn.addEventListener('click', () => {
+  nextBtn.style.backgroundColor = '#ba8d5b';
+  if (carouselList.scrollLeft == 0) {
+    prevBtn.style.backgroundColor = '#333';
+  } else {
+    carouselList.scrollLeft -= 215;
+  }
+});
